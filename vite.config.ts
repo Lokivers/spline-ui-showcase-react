ad497cb062703cb4f27180cd6b8222d4b6e87cf9
Lokivers <logeshvanitha96@gmail.com>
2025-07-30 19:58:54 +0530

    freshly added eth featured project and make them wiser

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,7 +14,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+<<<<<<< Updated upstream
   ],
+=======
+    mode === 'development' &&[]
+  ].filter(Boolean),
+>>>>>>> Stashed changes
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
